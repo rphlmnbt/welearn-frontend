d55a5a8b4b4f7d1fc55ffeccb9f2443e51332dff
rphlmnbt <rphlmnbt@gmail.com>
2022-02-18 18:19:37 +0800

    Redesigned dashboard

--- screens/FindPartner.js
+++ screens/FindPartner.js
@@ -4,7 +4,6 @@ import Background from '../assets/images/find-bg.svg'
 import AvatarImg from '../assets/images/avatar.png'
 import AppLoading from 'expo-app-loading';
 import * as Progress from 'react-native-progress';
-import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
