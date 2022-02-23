db34e0427c6515a5f33d3e76dfe904413d45bcc3
Glenn-Uy-Cana <glennuycana@gmail.com>
2022-02-23 22:05:48 +0800

    Updating UserReducer

--- actions/userActions.js
+++ actions/userActions.js
@@ -1,6 +1,12 @@
-import { LOGIN } from "../constants";
+import { LOGIN, SET_INTEREST } from "../constants";
+
 
 export const logIn = (values) => ({
     type: LOGIN,
+    payload: values
+})
+
+export const setInterest = (values) => ({
+    type: SET_INTEREST,
     payload: values
 })
--- reducers/userReducer.js
+++ reducers/userReducer.js
@@ -1,12 +1,35 @@
-import { LOGIN } from "../constants";
+import { LOGIN, SET_INTEREST } from "../constants";
 
 const initialState = {
-    user: ''
-}
+        uuid_user: '',
+        email: '',
+        first_name: '',
+        last_name: '',
+        gender: '',
+        contact_number: '',
+        university: '',
+        course: '',
+        year_level: '',
+        interest: '',
+        stats: [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        ],
+    }
 
 const userReducer = (state = initialState, action) => {
     switch(action.type) {
         case LOGIN:
+            return {
+                ...state,
+                user:action.payload
+            };
+         case SET_INTEREST:
             return {
                 ...state,
                 user:action.payload
