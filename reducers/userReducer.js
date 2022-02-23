7b8d9da6cf20859825d898f2008968905e34f27b
rphlmnbt <rphlmnbt@gmail.com>
2022-02-23 22:25:15 +0800

    Modified reducer

--- reducers/userReducer.js
+++ reducers/userReducer.js
@@ -1,38 +1,42 @@
 import { LOGIN, SET_INTEREST } from "../constants";
 
 const initialState = {
-        uuid_user: '',
-        email: '',
-        first_name: '',
-        last_name: '',
-        gender: '',
-        contact_number: '',
-        university: '',
-        course: '',
-        year_level: '',
-        interest: '',
-        stats: [
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0
-        ],
-    }
+    uuid_user: '',
+    email: '',
+    first_name: '',
+    last_name: '',
+    gender: '',
+    contact_number: '',
+    university: '',
+    course: '',
+    year_level: '',
+    interest: '',
+    stats: [
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+    ],
+}
 
 const userReducer = (state = initialState, action) => {
     switch(action.type) {
         case LOGIN:
             return {
                 ...state,
-                user:action.payload
+                uuid_user: action.payload.uuid_user,
+                email: action.payload.email,
+                first_name:action.payload.first_name,
+                last_name:action.payload.last_name
+                //ITUOLOY MO
             };
          case SET_INTEREST:
             return {
                 ...state,
-                user:action.payload
+                interest:action.payload.interest
             };
         default:
             return state;
