2e2afe199cf62b9e70ec27487f96199988a29bf1
rphlmnbt <rphlmnbt@gmail.com>
2022-03-03 11:50:06 +0800

    Created ML service file.

---
+++ services/ml.service.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import {API_URL} from '@env'
+
+const ML_URL = API_URL + '/ml'
+
+const addToDataset = (
+    uuid_user,
+    stats,
+    out
+) => {
+    return axios.post(ML_URL+'/add',{
+        uuid_user,
+        stats,
+        out
+    }).then(response => {
+        console.log(response)
+    })
+    .catch(error => {
+        //TODO: handle the error when implemented
+    })
+}
