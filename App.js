892062e17daf643071c60bc020a61632ee19af0d
rphlmnbt <rphlmnbt@gmail.com>
2022-03-23 23:34:52 +0800

    Set up asset caching and deleted unnecessary images.

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect, useRef} from 'react';
+import AppLoading from 'expo-app-loading';
+import cacheAssetsAsync from './assets/cacheAssetAsync';
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
 import { NavigationContainer } from '@react-navigation/native';
@@ -8,7 +10,6 @@ import SecondaryLogo from './components/SecondaryLogo';
 import SecondaryLogoBlack from './components/SecondaryLogoBlack';
 import BackWhite from './components/BackWhite';
 import BackBlack from './components/BackBlack';
-import PickStudyRoom from './screens/PickStudyRoom';
 import store from './store/store';
 import { Provider } from 'react-redux';
 import LoginHome from './screens/Login/LoginHome';
@@ -40,6 +41,7 @@ import UserFinishedSessionDetails from './screens/User/UserFinishedSessionDetail
 import UserReviewPartners from './screens/User/UserReviewPartners';
 import UserSessionType from './screens/User/UserSessionType';
 import UserInvitationDetails from './screens/User/UserInvitationDetails';
+import Loading from './components/Loading';
 
 const Stack = createNativeStackNavigator();
 
@@ -55,8 +57,10 @@ export default function App() {
   const [notification, setNotification] = useState(false);
   const notificationListener = useRef();
   const responseListener = useRef();  
+  const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
+
+  useEffect(async () => {
     // This listener is fired whenever a notification is received while the app is foregrounded
     notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
       setNotification(notification);
@@ -67,197 +71,231 @@ export default function App() {
       console.log(response);
     });
 
+    //caching assets
+    try {
+      await cacheAssetsAsync({
+        images: [
+          require('./assets/images/accept.png'),
+          require('./assets/images/card-bg1.png'),
+          require('./assets/images/card-bg2.png'),
+          require('./assets/images/check-button.png'),
+          require('./assets/images/home.png'),
+          require('./assets/images/logo2.png'),
+          require('./assets/images/logo-header1.png'),
+          require('./assets/images/logout.png'),
+          require('./assets/images/next.png'),
+          require('./assets/images/remove.png'),
+          require('./assets/images/people-fill.png'),
+          require('./assets/images/room.png'),
+          require('./assets/images/room-alt.png'),
+          require('./assets/images/settings.png'),
+          require('./assets/images/table.png'),
+          require('./assets/images/user-plus.png'),
+          require('./assets/images/wl-black.png'),
+          require('./assets/images/wl-logo.png'),
+          require('./assets/images/wl-logo2.png'),
+          require('./assets/images/wl-white.png'),
+        ],
+      });
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false)
+    }
+
+    //setting device for notif subscription
     return () => {
       Notifications.removeNotificationSubscription(notificationListener.current);
       Notifications.removeNotificationSubscription(responseListener.current);
     };
   }, []);
 
-  return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={Header}
-          initialRouteName="LoginHome"
-        >
-          <Stack.Screen 
-            name="LoginHome" 
-            component={LoginHome} 
-            options = {{headerTitle: () => null}}
-          />
-          <Stack.Screen 
-            name="LoginEmail" 
-            component={LoginEmail} 
-            options = {{headerTitle: () => null,
-              headerRight: () => <SecondaryLogo />,
-              headerLeft: () => <BackWhite />}}
-          />
-          <Stack.Screen 
-            name="LoginMobile" 
-            component={LoginMobile} 
-            options = {{headerTitle: () => null,
-              headerRight: () => <SecondaryLogoBlack />,
+  if (loading) {
+    return(
+      <Loading/>
+    )
+  } else {
+    return (
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={Header}
+            initialRouteName="LoginHome"
+          >
+            <Stack.Screen 
+              name="LoginHome" 
+              component={LoginHome} 
+              options = {{headerTitle: () => null}}
+            />
+            <Stack.Screen 
+              name="LoginEmail" 
+              component={LoginEmail} 
+              options = {{headerTitle: () => null,
+                headerRight: () => <SecondaryLogo />,
+                headerLeft: () => <BackWhite />}}
+            />
+            <Stack.Screen 
+              name="LoginMobile" 
+              component={LoginMobile} 
+              options = {{headerTitle: () => null,
+                headerRight: () => <SecondaryLogoBlack />,
+                headerLeft: () => <BackBlack />}}
+            />
+            <Stack.Screen 
+              name="LoginMobilePin" 
+              component={SignUpMobilePin} 
+              options = {{headerTitle: () => null,
+                headerRight: () => <SecondaryLogoBlack />,
+                headerLeft: () => <BackBlack />}}
+            />
+            <Stack.Screen 
+              name="SignUpPersonal" 
+              component={SignUpPersonal} 
+              options = {{headerTitle: () => null}}
+            />
+            <Stack.Screen 
+              name="SignUpBirth" 
+              component={SignUpBirth} 
+              options = {{headerTitle: () => null}}
+            />
+            <Stack.Screen 
+              name="SignUpEmail" 
+              component={SignUpEmail} 
+              options = {{headerTitle: () => null}}
+            />
+            <Stack.Screen 
+              name="SignUpContact" 
+              component={SignUpContact} 
+              options = {{headerTitle: () => null}}
+            />
+            <Stack.Screen 
+              name="SignUpSchool" 
+              component={SignUpSchool} 
+              options = {{headerTitle: () => null}}
+            />
+            <Stack.Screen 
+              name="SignUpCourse" 
+              component={SignUpCourse} 
+              options = {{headerTitle: () => null}}
+            />
+            <Stack.Screen 
+              name="SignUpSurveyIntro" 
+              component={SignUpSurveyIntro} 
+              options = {{headerTitle: () => null,
+                headerRight: () => <SecondaryLogoBlack />,
+                headerLeft: () => <BackBlack />}}
+            />
+            <Stack.Screen 
+              name="SignUpSurvey" 
+              component={SignUpSurvey} 
+              options = {{headerTitle: () => null,
+                headerLeft: () => <BackBlack />}}
+            />
+            <Stack.Screen 
+              name="UserDashboard" 
+              component={UserDashboard} 
+              options = {{headerTitle: () => null}}
+            />
+            <Stack.Screen 
+              name="UserFindPartner" 
+              component={UserFindPartner} 
+              options = {{headerTitle: () => null,
+                headerRight: () => <SecondaryLogo />,
+                headerLeft: () => <BackWhite />}}
+            />
+            <Stack.Screen 
+              name="UserCreateSession" 
+              component={UserCreateSession} 
+              options = {{headerTitle: () => null,
+                headerLeft: () => <BackWhite />}}
+            />
+            <Stack.Screen 
+              name="UserRequests" 
+              component={UserRequests} 
+              options = {{headerTitle: () => null,
               headerLeft: () => <BackBlack />}}
-          />
-          <Stack.Screen 
-            name="LoginMobilePin" 
-            component={SignUpMobilePin} 
-            options = {{headerTitle: () => null,
-              headerRight: () => <SecondaryLogoBlack />,
+            />
+            <Stack.Screen 
+              name="UserPartnerDetails" 
+              component={UserPartnerDetails} 
+              options = {{headerTitle: () => null,
+                headerRight: () => <SecondaryLogo />,
+                headerLeft: () => <BackWhite />}}
+            />
+            <Stack.Screen 
+              name="UserReviewPartners" 
+              component={UserReviewPartners} 
+              options = {{headerTitle: () => null,
+                headerRight: () => <SecondaryLogo />,
+                headerLeft: () => <BackWhite />}}
+            />
+            <Stack.Screen
+              name="UserReservations" 
+              component={UserReservations} 
+              options = {{headerTitle: () => null,
               headerLeft: () => <BackBlack />}}
-          />
-          <Stack.Screen 
-            name="SignUpPersonal" 
-            component={SignUpPersonal} 
-            options = {{headerTitle: () => null}}
-          />
-          <Stack.Screen 
-            name="SignUpBirth" 
-            component={SignUpBirth} 
-            options = {{headerTitle: () => null}}
-          />
-          <Stack.Screen 
-            name="SignUpEmail" 
-            component={SignUpEmail} 
-            options = {{headerTitle: () => null}}
-          />
-          <Stack.Screen 
-            name="SignUpContact" 
-            component={SignUpContact} 
-            options = {{headerTitle: () => null}}
-          />
-          <Stack.Screen 
-            name="SignUpSchool" 
-            component={SignUpSchool} 
-            options = {{headerTitle: () => null}}
-          />
-          <Stack.Screen 
-            name="SignUpCourse" 
-            component={SignUpCourse} 
-            options = {{headerTitle: () => null}}
-          />
-          <Stack.Screen 
-            name="SignUpSurveyIntro" 
-            component={SignUpSurveyIntro} 
-            options = {{headerTitle: () => null,
-              headerRight: () => <SecondaryLogoBlack />,
-              headerLeft: () => <BackBlack />}}
-          />
-          <Stack.Screen 
-            name="SignUpSurvey" 
-            component={SignUpSurvey} 
-            options = {{headerTitle: () => null,
-              headerLeft: () => <BackBlack />}}
-          />
-          <Stack.Screen 
-            name="UserDashboard" 
-            component={UserDashboard} 
-            options = {{headerTitle: () => null}}
-          />
-          <Stack.Screen 
-            name="UserFindPartner" 
-            component={UserFindPartner} 
-            options = {{headerTitle: () => null,
-              headerRight: () => <SecondaryLogo />,
-              headerLeft: () => <BackWhite />}}
-          />
-          <Stack.Screen 
-            name="UserCreateSession" 
-            component={UserCreateSession} 
-            options = {{headerTitle: () => null,
-              headerLeft: () => <BackWhite />}}
-          />
-          <Stack.Screen 
-            name="PickStudyRoom" 
-            component={PickStudyRoom} 
-            options = {{headerTitle: () => null}}
-          />
-          <Stack.Screen 
-            name="UserRequests" 
-            component={UserRequests} 
-            options = {{headerTitle: () => null,
-            headerLeft: () => <BackBlack />}}
-          />
-          <Stack.Screen 
-            name="UserPartnerDetails" 
-            component={UserPartnerDetails} 
-            options = {{headerTitle: () => null,
-              headerRight: () => <SecondaryLogo />,
-              headerLeft: () => <BackWhite />}}
-          />
-          <Stack.Screen 
-            name="UserReviewPartners" 
-            component={UserReviewPartners} 
-            options = {{headerTitle: () => null,
-              headerRight: () => <SecondaryLogo />,
-              headerLeft: () => <BackWhite />}}
-          />
-          <Stack.Screen
-            name="UserReservations" 
-            component={UserReservations} 
-            options = {{headerTitle: () => null,
-            headerLeft: () => <BackBlack />}}
-          />
-          <Stack.Screen
-            name="UserSettings" 
-            component={UserSettings} 
-            options = {{headerTitle: () => null}}
-          />
-          <Stack.Screen 
-            name="SignUpImageUpload" 
-            component={SignUpImageUpload} 
-            options = {{headerTitle: () => null}}
-          />
-
-           <Stack.Screen 
-            name="SignUpInterests" 
-            component={SignUpInterest} 
-            options = {{headerTitle: () => null}}
-          />
-          <Stack.Screen 
-            name="UserChooseSession" 
-            component={UserChooseSession} 
-            options = {{headerTitle: () => null}}
-          />
-          <Stack.Screen
-            name="UserAllReservations"
-            component={UserAllReservations}
-            options = {{headerTitle: () => null,
-              headerLeft: () => <BackBlack />}}
-          />
-          <Stack.Screen
-            name="UserFinishedSessions"
-            component={UserFinishedSessions}
-            options = {{headerTitle: () => null,
-              headerLeft: () => <BackBlack />}}
-          />
-          <Stack.Screen 
-            name="UserReservationDetails" 
-            component={UserReservationDetails} 
-            options = {{headerTitle: () => null}}
-          />
-          <Stack.Screen 
-            name="UserFinishedSessionDetails" 
-            component={UserFinishedSessionDetails} 
-            options = {{headerTitle: () => null}}
-          />
-          <Stack.Screen 
-            name="UserSessionType" 
-            component={UserSessionType} 
-            options = {{headerTitle: () => null,
-              headerRight: () => <SecondaryLogo />,
-              headerLeft: () => <BackWhite />}}
-          />
-          <Stack.Screen 
-            name="UserInvitationDetails" 
-            component={UserInvitationDetails} 
-            options = {{headerTitle: () => null}}
-          />
-
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
-
-  );
+            />
+            <Stack.Screen
+              name="UserSettings" 
+              component={UserSettings} 
+              options = {{headerTitle: () => null}}
+            />
+            <Stack.Screen 
+              name="SignUpImageUpload" 
+              component={SignUpImageUpload} 
+              options = {{headerTitle: () => null}}
+            />
+  
+             <Stack.Screen 
+              name="SignUpInterests" 
+              component={SignUpInterest} 
+              options = {{headerTitle: () => null}}
+            />
+            <Stack.Screen 
+              name="UserChooseSession" 
+              component={UserChooseSession} 
+              options = {{headerTitle: () => null}}
+            />
+            <Stack.Screen
+              name="UserAllReservations"
+              component={UserAllReservations}
+              options = {{headerTitle: () => null,
+                headerLeft: () => <BackBlack />}}
+            />
+            <Stack.Screen
+              name="UserFinishedSessions"
+              component={UserFinishedSessions}
+              options = {{headerTitle: () => null,
+                headerLeft: () => <BackBlack />}}
+            />
+            <Stack.Screen 
+              name="UserReservationDetails" 
+              component={UserReservationDetails} 
+              options = {{headerTitle: () => null}}
+            />
+            <Stack.Screen 
+              name="UserFinishedSessionDetails" 
+              component={UserFinishedSessionDetails} 
+              options = {{headerTitle: () => null}}
+            />
+            <Stack.Screen 
+              name="UserSessionType" 
+              component={UserSessionType} 
+              options = {{headerTitle: () => null,
+                headerRight: () => <SecondaryLogo />,
+                headerLeft: () => <BackWhite />}}
+            />
+            <Stack.Screen 
+              name="UserInvitationDetails" 
+              component={UserInvitationDetails} 
+              options = {{headerTitle: () => null}}
+            />
+  
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+  
+    );
+  }
 }
 
